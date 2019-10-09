341f3aaa9c47e1552ff04414c5430cce325ff0fa
Semyon Efimov <zbr.efimov@musbooking.com>
2019-10-09 19:26:59 +0300

    Add project files.

---
+++ Services/BotService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NokilayBot.Services
+{
+    public class BotService : IBotService
+    {
+        //private readonly BotConfiguration _config;
+
+        public BotService(IOptions<BotConfiguration> config)
+        {
+            BotConfiguration _config = config.Value;
+            foreach (var bot in _config.BotInfo)
+            {
+                Client.Add(bot.BotToken, new TelegramBotClient(bot.BotToken));
+                BotDomain.Add(bot.BotToken, bot.BotDomain);
+            }
+        }
+
+        public Dictionary<string, TelegramBotClient> Client { get; } = new Dictionary<string, TelegramBotClient>();
+        public Dictionary<string, Guid> BotDomain { get; } = new Dictionary<string, Guid>();
+    }
+}
---
+++ Services/IBotService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NokilayBot.Services
+{
+    public interface IBotService
+    {
+        Dictionary<string, TelegramBotClient> Client { get; }
+        Dictionary<string, Guid> BotDomain { get; }
+    }
+}
